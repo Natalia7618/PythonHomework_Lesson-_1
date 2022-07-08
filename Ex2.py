8564c09da6b98a71e84e856b512666f2da00fd02
Natalia Kushnir <natalka7618@yandex.ru>
2022-07-08 13:33:16 +0300

    написана 2я программа

--- Ex2.py
+++ Ex2.py
@@ -1,1 +1,9 @@
-# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+x = 0
+y = 1
+z = 1
+left_str = not (x or y or z)
+right_str = not x and not y and not z
+statement_result = left_str == right_str
+print (statement_result)
